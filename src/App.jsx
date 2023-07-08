f657389acea2f6b415372eed1913fcfff1d3e6ba
HAYLEY_SERRA <HAYLEY.SERRA24@GMAIL.COM>
2023-07-08 15:50:08 -0400

    Changed structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import { useCookies } from 'react-cookie';
-import MainPage from './MainPage';
+import StartPage from './StartPage';
 
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [cookies, setCookie] = useCookies(['myCookie']);
-  const [showComponent, setShowComponent] = useState(false);
-
-  const handleButtonClick = () => {
-    setShowComponent(true);
-  }
-
-  useEffect(() => {
-    setCookie('myCookie', inputValue);
-  }, [inputValue, setCookie]);
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
   return (
-    <div>
-      <h1>Welcome! Please enter your name: </h1>
-      <input type="text" value={inputValue} onChange={handleInputChange} />
-      <h1>{inputValue}</h1>
-      {showComponent ? <MainPage /> : null}
-      <button onClick={handleButtonClick}>Get Started</button>
-    </div>
-  )
+    <StartPage />
+  );
 }
 
 export default App;
